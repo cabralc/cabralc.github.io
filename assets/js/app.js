410f77b784fc684a1d8f7e8e5dc263a6fa9b106d
Casey Cabral <ccabral@paylease.com>
2020-01-20 09:30:45 -0800

    updated address

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,10 @@
     var details = {
         name    : "Casey Cabral",
         dob     : "5/30/1987",
-        address : "2207 2nd Ave",
+        address : "1415 Gregory Street",
         city    : "San Diego",
         state   : "CA",
-        zip     : "92101",
+        zip     : "92102",
         phone   : "619-400-7506",
         email   : "casey.cabral@gmail.com",
         position: "Software Engineer",
